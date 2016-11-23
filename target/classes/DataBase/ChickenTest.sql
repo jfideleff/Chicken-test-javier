4cbb90dca44056880fe798c8da12601440c81f6e
jfideleff <jfideleff@hotmail.com>
2016-11-23 12:02:59 -0300

    sql update

--- target/classes/DataBase/ChickenTest.sql
+++ target/classes/DataBase/ChickenTest.sql
@@ -9,32 +9,7 @@ nameF varchar(50) not null
 
 create table Chicken(
 id bigint(10) auto_increment primary key,
-nameC varchar(20) not null,
 farmId bigint(10) not null,
+eggs bigint(10),
 foreign key (farmId) references Farm(id)
 );
-
-create table Egg (
-id bigint(10) primary key auto_increment,
-size varchar(15) not null,
-chickenId bigint(10) not null,
-foreign key (chickenId) references Chicken(id)
-);
-
-insert into Farm (nameF) values ('The good Farm'),('The bad farm'),('The ugly Farm');
-
-insert into Chicken (nameC,farmId) values
-('Rosa',1),
-('Perla',2),
-('Jazmin',3),
-('Azalea',2),
-('Violeta',1);
-
-insert into Egg (chickenId,size) values
-(1,'small'),
-(2,'big'),
-(3,'small'),
-(4,'big'),
-(5,'small'),
-(4,'big'),
-(2,'small');
